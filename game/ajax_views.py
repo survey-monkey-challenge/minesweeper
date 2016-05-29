a970e18d07a583172b1349d9d728b043e6f3b990
Ernesto Ferro <eferro@sightlineinnovation.com>
2016-05-28 23:57:40 -0500

    Moving the ajax views to their own module

---
+++ game/ajax_views.py
@@ -0,0 +1,42 @@
+import json
+
+from django.core import signing
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404
+
+from .models import Game
+
+
+signer = signing.Signer()
+
+
+def sweep_view(request, signed_id):
+    return process_action_request(request, signed_id, Game.sweep_cell, False)
+
+
+def flag_view(request, signed_id):
+    return process_action_request(request, signed_id, Game.flag_cell, True)
+
+
+def process_action_request(request, signed_id, action, hide_mine_counter=True):
+    if not request.is_ajax() or not request.POST:
+        raise HttpResponseBadRequest()
+
+    game_id = signer.unsign(signed_id)
+    game = get_object_or_404(Game, pk=game_id)
+    x = int(request.POST['x'])
+    y = int(request.POST['y'])
+    is_game_over, win, cells = action(game, x, y)
+    data = {
+        'is_game_over': is_game_over,
+        'win': win,
+        'cells': [
+            {
+                'x': cell.x,
+                'y': cell.y,
+                'nearby_mine_counter': cell.nearby_mine_counter if not hide_mine_counter else None,
+                'css_class': cell.display.name,
+            } for cell in cells
+        ]
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from . import views, ajax_views
 
 
 app_name = 'game'
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.CreateGameView.as_view(), name='create'),
     url(r'^ranking/$', views.RankingView.as_view(), name='ranking'),
     url(r'^match/(?P<signed_id>.+)/$', views.GameView.as_view(), name='match'),
-    url(r'^sweep/(?P<signed_id>.+)/$', views.sweep_view, name='sweep'),
-    url(r'^flag/(?P<signed_id>.+)/$', views.flag_view, name='flag'),
+    url(r'^sweep/(?P<signed_id>.+)/$', ajax_views.sweep_view, name='sweep'),
+    url(r'^flag/(?P<signed_id>.+)/$', ajax_views.flag_view, name='flag'),
 ]
