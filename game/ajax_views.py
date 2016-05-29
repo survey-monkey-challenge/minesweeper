2a63423e19b8604d530d78c021ca00a7dec683e1
Ernesto Ferro <eferro@sightlineinnovation.com>
2016-05-29 11:31:02 -0500

    Replaced the javascript html manipulation in the ajax calls with server side rendering. It's DRYer more and the ajax calls are a bit simpler

--- game/ajax_views.py
+++ game/ajax_views.py
@@ -3,6 +3,7 @@ import json
 from django.core import signing
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 
 from .models import Game
 
@@ -11,17 +12,17 @@ signer = signing.Signer()
 
 
 def sweep_view(request, signed_id):
-    return process_action_request(request, signed_id, Game.sweep_cell, False)
+    return process_action_request(request, signed_id, Game.sweep_cell)
 
 
 def flag_view(request, signed_id):
-    return process_action_request(request, signed_id, Game.flag_cell, True)
+    return process_action_request(request, signed_id, Game.flag_cell)
 
 
-def process_action_request(request, signed_id, action, hide_mine_counter=True):
+def process_action_request(request, signed_id, action):
     '''
     Both sweep and flag actions are pretty much identical except for the method
-    they call on the game model object, and whether it should return nearby_mine_counter.
+    they call on the game model object.
     That's why this function was created. It can be seem as a higher-order function.
     '''
     if not request.is_ajax() or not request.POST:
@@ -39,8 +40,7 @@ def process_action_request(request, signed_id, action, hide_mine_counter=True):
             {
                 'x': cell.x,
                 'y': cell.y,
-                'nearby_mine_counter': cell.nearby_mine_counter if not hide_mine_counter else None,
-                'css_class': cell.display.name,
+                'html': render_to_string('cell.html', {'cell': cell})
             } for cell in cells
         ]
     }
